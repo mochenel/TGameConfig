3a410b202b2b86e06b0c5ac0ff26075ea0672d64
mochenel <freddy980404@gmail.com>
2020-10-03 14:17:32 +0200

    revert

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 // home route
-app.get("./", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home");
 });
 
